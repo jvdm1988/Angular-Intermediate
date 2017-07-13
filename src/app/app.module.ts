d6e4da456b93423fe90977b4ba676df3f65093fe
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-13 15:09:14 -0400

    Forms Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+// You need "FormsModule" to use "ngModel" in your components
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
