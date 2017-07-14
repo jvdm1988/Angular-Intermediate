2dfba26e1766e4c63949bd9d19dd3314e647033f
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-14 16:57:39 -0400

    contacts id route

--- src/app/contact-stuff/contact-details/contact-details.component.ts
+++ src/app/contact-stuff/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-contact-details',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-details.component.css']
 })
 export class ContactDetailsComponent implements OnInit {
+  myContactId: number;
 
-  constructor() { }
+    // Tell Angular we want to use ActivatedRoute in our component
+  constructor(
+    private routeThang: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    // When the component is loaded, retrieve the URL params
+
+    // req.params.myId
+    this.routeThang.params.subscribe((actualParams) => {
+      this.myContactId = actualParams.myId;
+    });
   }
 
 }
